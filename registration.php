f0726e1281c4bce5d8ecc67dbd4c1199497fd372
Martin Zwernemann <52236002+Zwernemann@users.noreply.github.com>
2026-03-24 14:13:31 +0100

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright 2026 Zwernemann
+ * All Rights Reserved.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Zwernemann_MinResolver',
+    __DIR__
+);
